d1d94ff608abaf81c825a7ab20101d140eb68d0d
davidghooost <davidghooost@hotmail.com>
2018-08-31 20:52:12 +0800

    try

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -136,18 +136,26 @@ BOOTSTRAP3 = {
 
 # Heroku settings
 cwd = os.getcwd()
+print("--- CWD ---\n", cwd, "\n---\n")
 if cwd == '/app' or cwd[:4] == '/tmp':
     import dj_database_url
+
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
     }
 
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-    ALLOWED_HOSTS =['*']
+    # Only allow heroku to host the project.
+    ALLOWED_HOSTS = ['*']
+    DEBUG = True
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

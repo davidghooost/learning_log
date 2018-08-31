3c74b0d90c7e2ab3675e9e67809db8f855bfc5a4
davidghooost <davidghooost@hotmail.com>
2018-08-31 20:41:55 +0800

    try

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,7 +147,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     ALLOWED_HOSTS =['*']
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static')
     )
